b47616dbe9e4b4c479f205c866644ea934188b40
nakielsh <hubert.nakielski@gmail.com>
2021-10-28 12:16:51 +0200

    added start() method, which starts the server

--- src/com/stratum/uiserver/UIServerApp.java
+++ src/com/stratum/uiserver/UIServerApp.java
@@ -5,6 +5,12 @@ import com.stratum.uiserver.graphics.ColorUtil;
 import com.stratum.uiserver.graphics.Graphics;
 import com.stratum.uiserver.graphics.Surface;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class UIServerApp {
     public static void testDraw(IFramebuffer framebuffer) {
         Surface surf = new Surface();
@@ -17,25 +23,51 @@ public class UIServerApp {
     }
 
     public static void main(String[] argv) {
-        if (argv.length == 1) {
-            IFramebuffer framebuffer;
+        try {
+            if (argv.length == 1) {
+                IFramebuffer framebuffer;
 
-            if (argv[0].equals("fb1")) {
-                framebuffer = new RPiFramebuffer();
-            } else if (argv[0].equals("swing")) {
-                framebuffer = new SwingFramebuffer();
+                if (argv[0].equals("fb1")) {
+                    framebuffer = new RPiFramebuffer();
+                } else if (argv[0].equals("swing")) {
+                    framebuffer = new SwingFramebuffer();
+                } else {
+                    System.out.println("Invalid mode.");
+                    return;
+                }
+
+                System.out.println(43);
+                testDraw(framebuffer);
+                start(50666);
             } else {
-                System.out.println("Invalid mode.");
-                return;
+                System.out.println("Usage: opal [mode]");
+                System.out.println("Modes:");
+                System.out.println("\tswing - display framebuffer in a window");
+                System.out.println("\tfb1 - send framebuffer to /dev/fb1 (RPi only)");
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-            System.out.println(43);
-            testDraw(framebuffer);
-        } else {
-            System.out.println("Usage: opal [mode]");
-            System.out.println("Modes:");
-            System.out.println("\tswing - display framebuffer in a window");
-            System.out.println("\tfb1 - send framebuffer to /dev/fb1 (RPi only)");
+    }
+
+    public static void start(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+
+                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream out = new DataOutputStream((clientSocket.getOutputStream()));
+                byte[] tablica = new byte[5];
+                in.readFully(tablica);
+
+                System.out.println(Arrays.toString(tablica));
+                out.write("dostalem".getBytes(StandardCharsets.UTF_8));
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
