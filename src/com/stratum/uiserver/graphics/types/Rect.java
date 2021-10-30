e8062caf724ebdadd393d5b877af4c364cd45a65
scintilla4evr <kacper17@outlook.com>
2021-10-30 21:40:47 +0200

    Added drawing methods for lines, polygons, ellipses and quadratic Beziers

---
+++ src/com/stratum/uiserver/graphics/types/Rect.java
@@ -0,0 +1,31 @@
+package com.stratum.uiserver.graphics.types;
+
+public class Rect {
+    private final int x;
+    private final int y;
+    private final int width;
+    private final int height;
+
+    public Rect(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public int width() {
+        return width;
+    }
+
+    public int height() {
+        return height;
+    }
+}
